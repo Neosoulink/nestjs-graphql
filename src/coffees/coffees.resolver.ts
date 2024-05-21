b21c3f51cb593d66376be3b885e1566a3659b16a
Nathan Mande <neodarksoulink@gmail.com>
2024-05-21 16:41:56 +0200

    refactor: passing agrs returning data by id

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,4 +1,5 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, ID, Query, Resolver } from '@nestjs/graphql';
+import { ParseIntPipe } from '@nestjs/common';
 
 import { Coffee } from './entities/coffee.entity/coffee.entity';
 
@@ -7,5 +8,10 @@ export class CoffeesResolver {
   @Query(() => [Coffee], { name: 'coffees' })
   findAll() {
     return [];
+  }
+
+  @Query(() => Coffee, { name: 'coffee', nullable: true })
+  findOne(@Args('id', { type: () => ID }, ParseIntPipe) id: number) {
+    return null;
   }
 }
