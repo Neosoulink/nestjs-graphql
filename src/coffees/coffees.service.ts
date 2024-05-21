f421dc8f668b9081eb664dde591ac377dd737f6f
Nathan Mande <neodarksoulink@gmail.com>
2024-05-21 19:34:07 +0200

    feat: using repository to access DB

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UserInputError } from '@nestjs/apollo';
+import { InjectRepository } from '@nestjs/typeorm';
 
 import { CreateCoffeeInput } from './dto/create-coffee.input/create-coffee.input';
+import { Coffee } from './entities/coffee.entity/coffee.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+
   async findAll() {
-    return [];
+    return this.coffeeRepository.find();
   }
 
   async findOne(id: number) {
-    return null;
+    const coffee = await this.coffeeRepository.findOne({ where: { id } });
+
+    if (!coffee) throw new UserInputError(`Coffee #${id} not found`);
+
+    return coffee;
   }
 
   async create(createCoffeeInput: CreateCoffeeInput) {
-    return null;
+    const coffee = this.coffeeRepository.create(createCoffeeInput);
+
+    return this.coffeeRepository.save(coffee);
   }
 }
