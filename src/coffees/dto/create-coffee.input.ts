1ce524c3c7c44fa2bc8b2747b12a61bf1150cfad
Nathan Mande <neodarksoulink@gmail.com>
2024-05-24 23:30:35 +0200

    docs: annotate appropriate import

--- src/coffees/dto/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input.ts
@@ -3,6 +3,7 @@ import { MinLength } from 'class-validator';
 
 import { CoffeeType } from '../../common/enums/coffee-type.enum';
 
+// Should extends the `GraphQLType#CreateCoffeeInput` in case of schema-first approach
 @InputType({ description: 'Create coffee input object type' })
 export class CreateCoffeeInput {
   @MinLength(3)
--- src/coffees/dto/update-coffee.input.ts
+++ src/coffees/dto/update-coffee.input.ts
@@ -1,5 +1,6 @@
 import { InputType, PartialType } from '@nestjs/graphql';
 import { CreateCoffeeInput } from './create-coffee.input';
 
+// Should extends the `GraphQLType#UpdateCoffeeInput` in case of schema-first approach
 @InputType()
 export class UpdateCoffeeInput extends PartialType(CreateCoffeeInput) {}
